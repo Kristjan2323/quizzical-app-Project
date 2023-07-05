1cbcf36bede58ef6d63bb566baa4af0966b45b61
Kristi Ballolli <kristianballolli1@gmail.com>
2023-07-05 22:17:05 +0200

    created logics: getRightAnswers , check if all questions have one alternative selected

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import { nanoid } from 'nanoid'
 
 export default function App() {
   const [questions,setQuestion] = React.useState([])
-
+  const [allQuestionsAreChecked, setAllQuestionChecked] = React.useState(false)
+  const [quizzSatistics, setQuizzSatistics] = React.useState("");
   
 
     React.useEffect(() => {
       GetQuestionData();
     }, []);
-  
+    
+
     // Rest of your component code...
   
     React.useEffect(() => {
@@ -49,25 +51,53 @@ export default function App() {
     const decodedCorrectAnswer = decodeHTML(result.correct_answer);
     const decodeAllAnswers = decodeHTML(allAnswers)
 
+     const addIdForEachAlternative = allAlternativesInArray.map((alternative) =>{
+     return{
+      id: nanoid(),
+      isSelected : false,
+      alternative : alternative
+     }
+  
+
+     });
       return{
-        id: nanoid(),
-        isSelected : false,
+        id: nanoid(),     
         questionTitle: decodedQuestion,
         wrongAnswers : decodedIncorrectAnswers ,
         rightAnswers :decodedCorrectAnswer,
-        allAnswers : allAlternativesInArray
+        allAnswers : addIdForEachAlternative
       };
-    
-    });
+     });
    
    return randomQuestion
    
    }
   
+   function handleSelectedAlternative(selectedAlternativeId,questionId) {
+    setQuestion((questions) =>
+      questions?.map((question) => {
+        return {
+          ...question,
+           
+          allAnswers: question.allAnswers?.map((alternative) => {
+           
+            if(question.id === questionId && alternative.id !== selectedAlternativeId){
+              return { ...alternative, isSelected:false};
+            }
+            if (alternative.id === selectedAlternativeId) {
 
+              return { ...alternative, isSelected: !alternative.isSelected };
+
+            }
+           
+           return alternative;
+          })
+        };
+      })
+    );
+  }
     
    function GetQuestionData(){
-   
     fetch('https://opentdb.com/api.php?amount=5&type=multiple')
     .then(response => response.json())
     .then(data => {
@@ -76,17 +106,86 @@ export default function App() {
       
     })
    }
+   function getAllSelectedAlternatives(){
+    const selectedAlternatives = questions.map(question =>
+      question.allAnswers.filter(alternative => alternative.isSelected ===true)
+
+    )
+    const onlyAlternative = selectedAlternatives?.map(selectedAlternative =>
+      selectedAlternative.alternative )
+    console.log(selectedAlternatives)
+    return selectedAlternatives
+   }
+   
+   function getAllRightAnswersFromQuestions(){
+    const rightAnswers = questions?.map(question =>
+      question.rightAnswers
+    )
+    console.log(rightAnswers)
+    return rightAnswers
+   }
+   //button click
+   function handleAnswers(){
+    const check = checkIfAllAlternativesAreAllSelected();
+    getAllRightAnswersFromQuestions();
+    if(check === true){
+      let correctAnswers = 0;
+      let incorrectAnswers = 0;
+      const selectedAlternatives =  getAllSelectedAlternatives();
+      const rightAnswers = getAllRightAnswersFromQuestions();
+      for(let i = 0 ; i<rightAnswers.length; i++){
+        const see = selectedAlternatives[i].alternative
+        const see2 = rightAnswers[i]
+          if(selectedAlternatives[i][0].alternative ===rightAnswers[i] ){
+            correctAnswers += 1
+          }
+          else{
+            incorrectAnswers += 1
+          }
+      }
+      let correctAnswersSt = correctAnswers.toString();
+      let incorrectAnswersSt = incorrectAnswers.toString();
+      const quizzReport = correctAnswersSt + "/" + "5"
+      setQuizzSatistics(quizzReport)
+      setAllQuestionChecked(true)
+    }
+    else{
+      setAllQuestionChecked(false)
+    }
+   }
+
+   function checkIfAllAlternativesAreAllSelected(){
+   const selectedAlternatives =  getAllSelectedAlternatives();
+   let allSelected = true;
+     for(let i = 0; i<selectedAlternatives.length;i++ ){
+      if(selectedAlternatives[i].length === 0){
+        allSelected = false
+      }
+     }
+     console.log(allSelected)
+     return allSelected
+   }
 
   const questionElement = questions?.map(question => (
-    <Questions key = {question.id} questionTitle= {question.questionTitle}  alternatives = {question.allAnswers} />
+    <Questions key = {nanoid()} id = {question.id} alternativeId= {question.allAnswers?.map(alternative =>(alternative.id))} 
+     questionTitle= {question.questionTitle}  
+    alternatives = {question.allAnswers} isSelected ={question.allAnswers?.map(alternative =>(alternative.isSelected))} 
+     handleSelectedAlternative = {(alternativeIdParameter,questionId) => handleSelectedAlternative(alternativeIdParameter,questionId)}
+     />
+    
   ))
+
 
   return (
     <div className="App">
    
   { questionElement}
   
-
+  <span >
+    <h3 className='validationAlternativesChecked'>
+      {allQuestionsAreChecked && quizzSatistics  }</h3>
+    <button className="btnCheckAnswers" onClick={handleAnswers} type="button" >Check Answers</button>
+    </span>
     </div>
   );
 }
--- src/Questions.js
+++ src/Questions.js
@@ -7,16 +7,24 @@ export default function Questions(props){
         const decodedString = parser.parseFromString(html, 'text/html').body.textContent;
         return decodedString;
       }
+
+      const styles = props.alternatives.map(alternative => ({
+        backgroundColor: alternative.isSelected ? "#59E391" : "white"
+      }));
+        
+      
      //create onClick button to change selected status
     return(
     <div className="main">
      <h2 className="questionTitle">{props.questionTitle}</h2>
  
-     <h4 className="alternatives ">{decodeHTML(props.alternatives[0])}</h4>
-     <h4 className="alternatives">{decodeHTML(props.alternatives[1])}</h4>
-     <h4 className="alternatives">{decodeHTML(props.alternatives[2])}</h4>
-     <h4 className="alternatives">{decodeHTML(props.alternatives[3])}</h4>
+     <h4 className="alternatives " style = {styles[0]} onClick={() => props.handleSelectedAlternative(props.alternativeId[0], props.id)}>{decodeHTML(props.alternatives[0].alternative)}</h4>
+      <h4 className="alternatives" style = {styles[1]}  onClick={() => props.handleSelectedAlternative(props.alternativeId[1], props.id)}>{decodeHTML(props.alternatives[1].alternative)}</h4>
+      <h4 className="alternatives" style = {styles[2]}  onClick={() => props.handleSelectedAlternative(props.alternativeId[2], props.id)}>{decodeHTML(props.alternatives[2].alternative)}</h4>
+      <h4 className="alternatives" style = {styles[3]}  onClick={() => props.handleSelectedAlternative(props.alternativeId[3], props.id)}>{decodeHTML(props.alternatives[3].alternative)}</h4>
+   
     </div>
+    
   )
 
 
